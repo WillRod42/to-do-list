756fdfc14fcf563f25901aef5dd92d8e0a51d2d2
William Rodriguez <william@rodriguez1.com>
2022-04-04 12:05:08 -0700

    Add HTML and UI logic for adding new tasks
    Co-authored-by: Meron G.Tekie <mtekiesd210309@gmail.com>
    Co-authored-by: Caleb Coughenour <CalebCoughenour@protonmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,27 @@ function ToDoList() {
 ToDoList.prototype.addTask = function(taskDesc) {
   if (!this.tasks[taskDesc]) {
     this.tasks[taskDesc] = new Task(taskDesc);
-  } 
+    return true;
+  } else {
+    return false;
+  }
 }
 
 ToDoList.prototype.removeTask = function(taskDesc) {
-  delete this.tasks[taskDesc];
+  if (this.tasks[taskDesc]) {
+    delete this.tasks[taskDesc];
+    return true;
+  } else {
+    return false;
+  }
 }
 
 ToDoList.prototype.finishTask = function(taskDesc) {
   if (this.tasks[taskDesc]) {
     this.tasks[taskDesc].finished = true;
+    return true;
+  } else {
+    return false;
   }
 }
 
@@ -22,3 +33,33 @@ function Task(taskDesc) {
   this.task = taskDesc;
   this.finished = false;
 }
+
+function 
+
+function createTaskUI(taskDesc) {
+  let task = $("<li></li>");
+  task.text(taskDesc);
+
+  let finishBtn = $("<button class='finish btn btn-success'>finish</button>");
+  let removeBtn = $("<button class='remove btn btn-danger'>remove</button>");
+
+  /*
+    Add click methods here
+  */
+
+  task.append(finishBtn);
+  task.append(removeBtn);
+
+  return task;
+}
+
+$(document).ready(function() {
+  let toDo = new ToDoList();
+
+  $("form").submit(function(e) {
+    e.preventDefault();
+    if (toDo.addTask($("#task").val())) {
+      $("#to-do-list ul").append(createTaskUI($("#task").val()));
+    }
+  });
+});
